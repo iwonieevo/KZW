fc85fd8f216f8f457f4679cd6d4fab51976d32f5
iwonieevo <iwo.chwiszczuk@gmail.com>
2026-03-20 00:01:06 +0100

    Refactor Task class and update scheduling algorithms for improved clarity and functionality

--- lista_1.py
+++ lista_1.py
@@ -3,16 +3,23 @@ from dataclasses import dataclass
 from collections import deque
 import heapq
 import copy
+from colors import color
 
 @dataclass(order=True)
 class Task():
-    task_id         : int     # j
-    processing_time : int     # p
-    release_time    : int = 0 # r
-    delivery_time   : int = 0 # q
+    j: int = -1 # Task ID
+    r: int = 0  # Release time
+    p: int = 0  # Processing time
+    q: int = 0  # Delivery time
 
     def __str__(self):
-        return f"Task(j={self.task_id}, r={self.release_time}, p={self.processing_time}, q={self.delivery_time})"
+        j_str = color(f"j={self.j}", fg="magenta", style="bold")
+        r_str = color(f"r={self.r}", fg="orange")
+        p_str = color(f"p={self.p}", fg=(78, 220, 78)) # bright green
+        q_str = color(f"q={self.q}", fg="yellow")
+
+        return f"[ {j_str} | {r_str} | {p_str} | {q_str} ]"
+    
     
 class Schedule:
     def __init__(self, tasks: list[Task]):
@@ -20,210 +27,212 @@ class Schedule:
         self.start_times      : list[int]  = []    # S
         self.completion_times : list[int]  = []    # C
         self.delivery_times   : list[int]  = []    # C + q
-        self.cmax             : int        = 0     # Cmax
+        self.c_max            : int        = 0     # C_max
 
     def __iter__(self):
-        return iter(self.tasks)
+        return iter(zip(self.tasks, self.start_times, self.completion_times, self.delivery_times))
 
     def compute(self):
         self.start_times      = []
         self.completion_times = []
         self.delivery_times   = []
-        self.cmax             = 0
+        self.c_max             = 0
 
         for i, task in enumerate(self.tasks):
-            start      : int = task.release_time if i == 0 else max(task.release_time, self.completion_times[i-1])
-            completion : int = start + task.processing_time
-            delivery   : int = completion + task.delivery_time
+            start      : int = task.r if i == 0 else max(task.r, self.completion_times[i-1])
+            completion : int = start + task.p
+            delivery   : int = completion + task.q
 
             self.start_times.append(start)
             self.completion_times.append(completion)
             self.delivery_times.append(delivery)
-            self.cmax = max(self.cmax, delivery)
+            self.c_max = max(self.c_max, delivery)
 
     def display(self):
-        rows = [
-            ["j"]         + [str(t.task_id) for t    in self.tasks],
-            ["S_j"]       + [str(s_i)       for s_i  in self.start_times],
-            ["C_j"]       + [str(c_i)       for c_i  in self.completion_times],
-            ["C_j + q_j"] + [str(cq_i)      for cq_i in self.delivery_times]
-        ]
-
-        col_width = max(len(cell) for row in rows for cell in row)
-        separator = "-+-".join("-" * col_width for _ in rows[0])
-
-        for row in rows:
-            print(" | ".join(f"{cell:>{col_width}}" for cell in row))
-            print(separator)
+        col_width = len(str(self.c_max))
+        label_width = 9
+        frame_color = "cyan"
+        hor_div = color("-"*(label_width + 1) + "+-" + "-+-".join("-" * col_width for _ in self.tasks) + "-+", fg=frame_color)
+        rows = [("j",         [t.j for t in self.tasks], "magenta"),
+                ("S_j",       self.start_times,          "blue"), 
+                ("C_j",       self.completion_times,     "green"), 
+                ("C_j + q_j", self.delivery_times,       "yellow")]
         
-        print(f"Cmax: {self.cmax}\n")
+        print(hor_div)
+        for label, row, fg_color in rows:
+            label_str = color(f"{label:^{label_width}}", fg=fg_color, style="bold")
+            vert_div = color(" | ", fg=frame_color)
+            row_str = vert_div.join(color(f"{cell:^{col_width}}", fg=fg_color) for cell in row)
+            
+            print(f"{label_str}{vert_div}{row_str}{vert_div}")
+            print(hor_div)
+        
+        print(color(f"C_max: {self.c_max}\n", fg="red", style="bold"))
 
-def schrage(tasks: list[Task]) -> tuple[list[Task], int]:
-    if not tasks:
+
+def schrage(J: list[Task]) -> tuple[list[Task], int]:
+    if not J:
         return [], 0
 
-    N        : deque                  = deque(sorted(tasks, key=lambda t: t.release_time))
+    N        : deque                  = deque(sorted(J, key=lambda t: t.r))
     G        : list[tuple[int, Task]] = []
-    t        : int                    = N[0].release_time
-    cmax     : int                    = 0
+    t        : int                    = N[0].r
+    c_max    : int                    = 0
     schedule : list[Task]             = []
 
     while G or N:
-        while N and N[0].release_time <= t:
+        while N and N[0].r <= t:
             task = N.popleft()
-            heapq.heappush(G, (-task.delivery_time, task))
+            heapq.heappush(G, (-task.q, task))
 
         if G:
             _, task = heapq.heappop(G)
             schedule.append(task)
 
-            t += task.processing_time
-            cmax = max(cmax, t + task.delivery_time)
+            t += task.p
+            c_max = max(c_max, t + task.q)
         else:
-            t = N[0].release_time
+            t = N[0].r
 
-    return schedule, cmax
+    return schedule, c_max
 
-def preemptive_schrage(tasks: list[Task]) -> int:
-    if not tasks:
+def preemptive_schrage(J: list[Task]) -> int:
+    if not J:
         return 0
     
-    N            : deque                  = deque(sorted(tasks, key=lambda t: t.release_time))
+    N            : deque                  = deque(sorted(J, key=lambda t: t.r))
     G            : list[tuple[int, Task]] = []
-    t            : int                    = N[0].release_time
-    cmax         : int                    = 0
-    current_task : Task                   = Task(task_id=-1, processing_time=0, release_time=0, delivery_time=float('inf'))
+    t            : int                    = N[0].r
+    c_max        : int                    = 0
+    current_task : Task                   = Task(j=-1, r=0, p=0, q=float('inf'))
     
     while G or N:
-        while N and N[0].release_time <= t:
+        while N and N[0].r <= t:
             task = N.popleft()
-            heapq.heappush(G, (-task.delivery_time, task))
+            heapq.heappush(G, (-task.q, task))
 
         if G:
             _, task = heapq.heappop(G)
             current_task = copy.deepcopy(task)
 
-            next_r = N[0].release_time if N else float('inf')
-            finish_curr = t + current_task.processing_time
-            dt = min(next_r, finish_curr) - t
+            next_r = N[0].r if N else float('inf')
+            finish_current = t + current_task.p
+            dt = min(next_r, finish_current) - t
             t += dt
-            current_task.processing_time -= dt
+            current_task.p -= dt
 
-            if current_task.processing_time > 0:
-                heapq.heappush(G, (-current_task.delivery_time, current_task))
+            if current_task.p > 0:
+                heapq.heappush(G, (-current_task.q, current_task))
             else:
-                cmax = max(cmax, t + task.delivery_time)
+                c_max = max(c_max, t + current_task.q)
         else:
-            t = N[0].release_time
+            t = N[0].r
 
-    return cmax
+    return c_max
+
+def carlier(J: list[Task], UB: int = float('inf')) -> tuple[list[Task], int]:
+    best_pi = None
+    pi, U = schrage(J)
+
+    if U < UB:
+        UB = U
+        best_pi = copy.deepcopy(pi)
+
+    sched = Schedule(pi)
+    sched.compute()
+
+    b = next(j for j, (_, _, _, d) in enumerate(sched) if d == sched.c_max)
+    a = next(j for j in range(b+1) if sum(t.p for t in pi[j:b+1]) + pi[j].r + pi[b].q == sched.c_max)
+    c = next((j for j in range(a, b+1) if pi[j].q < pi[b].q), None)
+
+    if c is None:
+        return best_pi, UB
+    
+    K = pi[c:b+1]
+
+    pi_branch = copy.deepcopy(pi)
+    r_tmp = pi[c].r
+    min_rK = min(t.r for t in K)
+    min_qK = min(t.q for t in K)
+    pi_branch[c].r = max(r_tmp, min_rK + min_qK)
+    LB = preemptive_schrage(pi_branch)
+    if LB < UB:
+        new_pi, UB = carlier(pi_branch, UB)
+        if new_pi:
+            best_pi = new_pi
+
+    pi_branch = copy.deepcopy(pi)
+    q_tmp = pi_branch[c].q
+    pi_branch[c].q = max(q_tmp, min_qK + sum(t.p for t in K))
+    LB = preemptive_schrage(pi_branch)
+    if LB < UB:
+        new_pi, UB = carlier(pi_branch, UB)
+        if new_pi:
+            best_pi = new_pi
+
+    return best_pi, UB
 
 def generate_random_tasks(n: int, Z: int) -> list[Task]:
     randGen = RandomNumberGenerator(Z)
-    tasks: list[Task] = [Task(task_id=j, processing_time=randGen.nextInt(1, 29)) for j in range(1, n+1)]
-    A: int = sum(task.processing_time for task in tasks)
+    tasks: list[Task] = [Task(j=j, p=randGen.nextInt(1, 29)) for j in range(1, n+1)]
+    A: int = sum(task.p for task in tasks)
     X: int = A
     for task in tasks:
-        task.release_time = randGen.nextInt(1, A)
+        task.r = randGen.nextInt(1, A)
     for task in tasks:
-        task.delivery_time = randGen.nextInt(1, X)
+        task.q = randGen.nextInt(1, X)
     
     return tasks
+
+def print_headline(headline: str, frame: str = "#", frame_width: int = 1, padding: int = 1, fg: str = "cyan", style: str = "underline+bold"):
+    div = color(frame * (len(headline) + 2 * (frame_width + padding)), fg=fg, style="bold")
+    frame = frame[0]
+
+    middle = (
+        color(frame * frame_width, fg=fg, style="bold") +
+        " " * padding +
+        color(headline.upper(), fg=fg, style=style) +
+        " " * padding +
+        color(frame * frame_width, fg=fg, style="bold")
+    )
     
+    # Print the framed headline
+    print("\n" + div)
+    print(middle)
+    print(div + "\n")
+
+        
 if __name__ == '__main__':
-    tasks = generate_random_tasks(n=6, Z=1)
-    # tasks = [
-    #     Task(task_id=1, release_time=1, processing_time=2, delivery_time=5),
-    #     Task(task_id=2, release_time=2, processing_time=3, delivery_time=4),
-    #     Task(task_id=3, release_time=8, processing_time=1, delivery_time=6),
-    #     Task(task_id=4, release_time=7, processing_time=2, delivery_time=3),
-    #     Task(task_id=5, release_time=6, processing_time=3, delivery_time=7),
-    #     Task(task_id=6, release_time=4, processing_time=4, delivery_time=1)
-    # ]
+    # Generating tasks
+    tasks = generate_random_tasks(n=20, Z=1)
+    print_headline("Generated tasks")
     for t in tasks:
         print(t)
     
+    # Original order schedule
     og_schedule = Schedule(tasks)
     og_schedule.compute()
+    print_headline("Schedule #1: Original Task Order")
     og_schedule.display()
 
+    # Schrage non-preemptive schedule
     schrage_schedule = Schedule(schrage(tasks)[0])
     schrage_schedule.compute()
+    print_headline("Schedule #2: Schrage (Non-Preemptive)")
     schrage_schedule.display()
-    print(preemptive_schrage(tasks))
+
+    # Schrage preemptive C_max
+    print_headline("Preemptive Schrage (C_max only)")
+    print(color(f"C_max: {preemptive_schrage(tasks)}\n", fg="red", style="bold"))
+
+    # Original tasks after preemptive shrage
+    print_headline("Original tasks after preemptive shrage (unchanged)")
     for t in tasks:
         print(t)
 
-    
-def preemptive_schrage(tasks: list[Task]) -> tuple[list[Task], int]:
-    # to be commited
-    return [], 0
+    # Carlier schedule
+    carlier_schedule = Schedule(carlier(tasks)[0])
+    carlier_schedule.compute()
+    print_headline("Schedule #3: Carlier")
+    carlier_schedule.display()
 
-
-
-def carlier(tasks: list[Task], UB: int) -> tuple[list[Task], int]:
-    best_solution = [None, float('inf')]
-    
-    def search(tasks: list[Task], UB: int) -> int:
-        pi, U = schrage(tasks)
-        
-        if U < best_solution[1]:
-            best_solution[0] = pi[:]
-            best_solution[1] = U
-        
-        sched = Schedule(pi)
-        sched.compute()
-        
-        b = -1
-        for j in range(len(pi)):
-            if sched.delivery_times[j] == sched.cmax:
-                b = j
-        
-        a = -1
-        for j in range(b + 1):
-            p_sum = sum(pi[k].processing_time for k in range(j, b + 1))
-            if sched.cmax == pi[j].release_time + p_sum + pi[b].delivery_time:
-                a = j
-                break
-        
-        if a == -1:
-            return best_solution[1]
-        
-        c = -1
-        for j in range(a, b):
-            if pi[j].delivery_time < pi[b].delivery_time:
-                c = j
-        
-        if c == -1:
-            return best_solution[1]
-        
-        K = list(range(c + 1, b + 1))
-        
-        r_min = min(pi[j].release_time for j in K)
-        q_min = min(pi[j].delivery_time for j in K)
-        p_sum = sum(pi[j].processing_time for j in K)
-        
-        old_r = pi[c].release_time
-        pi[c].release_time = max(pi[c].release_time, r_min + p_sum)
-        
-        _, LB = preemptive_schrage(pi)
-        
-        if LB < best_solution[1]:
-            search(pi, best_solution[1])
-            
-        pi[c].release_time = old_r
-        
-        old_q = pi[c].delivery_time
-        pi[c].delivery_time = max(pi[c].delivery_time, q_min + p_sum)
-        
-        _, LB = preemptive_schrage(pi)
-        
-        if LB < best_solution[1]:
-            search(pi, best_solution[1])
-            
-        pi[c].delivery_time = old_q
-        
-        return best_solution[1]
-    
-    search(tasks, UB)
-    return best_solution[0], best_solution[1]
