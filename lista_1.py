eaa9fdb5b03df95c52acf1c25c76d37e63bb4221
flis0 <szymek.flis@gmail.com>
2026-03-19 16:42:33 +0100

    removed the global var

--- lista_1.py
+++ lista_1.py
@@ -115,65 +115,70 @@ def preemptive_schrage(tasks: list[Task]) -> tuple[list[Task], int]:
     return [], 0
 
 
-best_solution = [None, float('inf')]
 
 def carlier(tasks: list[Task], UB: int) -> tuple[list[Task], int]:
-    pi, U = schrage(tasks)
+    best_solution = [None, float('inf')]
     
-    if U < best_solution[1]:
-        best_solution[0] = pi[:]
-        best_solution[1] = U
-    
-    sched = Schedule(pi)
-    sched.compute()
-    
-    b = -1
-    for j in range(len(pi)):
-        if sched.delivery_times[j] == sched.cmax:
-            b = j
-    
-    a = -1
-    for j in range(b + 1):
-        p_sum = sum(pi[k].processing_time for k in range(j, b + 1))
-        if sched.cmax == pi[j].release_time + p_sum + pi[b].delivery_time:
-            a = j
-            break
-    
-    if a == -1:
-        return best_solution[0], best_solution[1]
-    
-    c = -1
-    for j in range(a, b):
-        if pi[j].delivery_time < pi[b].delivery_time:
-            c = j
-    
-    if c == -1:
-        return best_solution[0], best_solution[1]
-    
-    K = list(range(c + 1, b + 1))
-    
-    r_min = min(pi[j].release_time for j in K)
-    q_min = min(pi[j].delivery_time for j in K)
-    p_sum = sum(pi[j].processing_time for j in K)
-    
-    old_r = pi[c].release_time
-    pi[c].release_time = max(pi[c].release_time, r_min + p_sum)
-    
-    _, LB = preemptive_schrage(pi)
-    
-    if LB < best_solution[1]:
-        carlier(pi, best_solution[1])
+    def search(tasks: list[Task], UB: int) -> int:
+        pi, U = schrage(tasks)
         
-    pi[c].release_time = old_r
-    
-    old_q = pi[c].delivery_time
-    pi[c].delivery_time = max(pi[c].delivery_time, q_min + p_sum)
-    
-    _, LB = preemptive_schrage(pi)
-    
-    if LB < best_solution[1]:
-        carlier(pi, best_solution[1])
+        if U < best_solution[1]:
+            best_solution[0] = pi[:]
+            best_solution[1] = U
         
-    pi[c].delivery_time = old_q
+        sched = Schedule(pi)
+        sched.compute()
+        
+        b = -1
+        for j in range(len(pi)):
+            if sched.delivery_times[j] == sched.cmax:
+                b = j
+        
+        a = -1
+        for j in range(b + 1):
+            p_sum = sum(pi[k].processing_time for k in range(j, b + 1))
+            if sched.cmax == pi[j].release_time + p_sum + pi[b].delivery_time:
+                a = j
+                break
+        
+        if a == -1:
+            return best_solution[1]
+        
+        c = -1
+        for j in range(a, b):
+            if pi[j].delivery_time < pi[b].delivery_time:
+                c = j
+        
+        if c == -1:
+            return best_solution[1]
+        
+        K = list(range(c + 1, b + 1))
+        
+        r_min = min(pi[j].release_time for j in K)
+        q_min = min(pi[j].delivery_time for j in K)
+        p_sum = sum(pi[j].processing_time for j in K)
+        
+        old_r = pi[c].release_time
+        pi[c].release_time = max(pi[c].release_time, r_min + p_sum)
+        
+        _, LB = preemptive_schrage(pi)
+        
+        if LB < best_solution[1]:
+            search(pi, best_solution[1])
+            
+        pi[c].release_time = old_r
+        
+        old_q = pi[c].delivery_time
+        pi[c].delivery_time = max(pi[c].delivery_time, q_min + p_sum)
+        
+        _, LB = preemptive_schrage(pi)
+        
+        if LB < best_solution[1]:
+            search(pi, best_solution[1])
+            
+        pi[c].delivery_time = old_q
+        
+        return best_solution[1]
     
+    search(tasks, UB)
     return best_solution[0], best_solution[1]
