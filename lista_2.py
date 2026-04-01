7680448d6e230c691bcb1d42a3af7f6a80587ba4
iwonieevo <iwo.chwiszczuk@gmail.com>
2026-04-01 15:49:47 +0200

    Add Task and TaskList classes with methods for task management and validation

---
+++ lista_2.py
@@ -0,0 +1,51 @@
+from RandomNumberGenerator import RandomNumberGenerator
+from dataclasses import dataclass
+# https://pypi.org/project/ansicolors/
+from colors import color
+
+@dataclass(frozen=True, kw_only=True, order=True)
+class Task:
+    j: int              # Task ID
+    p: list[int]        # Processing times: p[i] = time on machine i+1
+
+    @classmethod
+    def from_task(cls, task: Task, **kwargs):
+        return cls(
+            j=kwargs.get('j', task.j),
+            p=kwargs.get('p', task.p.copy()),
+        )
+    
+    def __copy__(self):
+        return Task(j=self.j, p=self.p.copy())
+    
+    def __deepcopy__(self, memo):
+        return Task(j=self.j, p=self.p.copy())
+
+    def __str__(self):
+        jStr = color(f"j={self.j}", fg="magenta", style="bold")
+        pStrs = " | ".join(
+            color(f"p{i+1}={pi}", fg="orange")
+            for i, pi in enumerate(self.p)
+        )
+        return f"[ {jStr} | {pStrs} ]"
+    
+@dataclass(kw_only=True)
+class TaskList:
+    m: int            # Number of machines
+    tasks: list[Task] # List of tasks
+
+    def __post_init__(self):
+        assert self.m > 0, "Number of machines must be a positive integer."
+        
+        for task in self.tasks:
+            assert(len(task.p) == self.m, f"Task {task.j} has {len(task.p)} operations, expected {self.m}")
+
+@dataclass(frozen=True)
+class Schedule:
+    tasks: list[Task]         # J
+    __S:     list[list[int]]  # S
+    __C:     list[list[int]]  # C
+    __cMax:  int              # C_max
+
+if __name__ == "__main__":
+    pass
