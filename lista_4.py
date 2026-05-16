3865316a1e35857ed624389b9e080834eca1d56c
iwonieevo <iwo.chwiszczuk@gmail.com>
2026-05-17 01:10:42 +0200

    checkpoint

--- lista_4.py
+++ lista_4.py
@@ -1,10 +1,9 @@
 from RandomNumberGenerator import RandomNumberGenerator
 from dataclasses import dataclass
-from typing import Iterator, Iterable
+from collections import deque
+from typing import Iterator, Iterable, Literal
 # https://pypi.org/project/ansicolors/
 from colors import color
-# https://pypi.org/project/rgb-gradient/
-from rgb_gradient import get_linear_gradient
 import math
 
 
@@ -65,6 +64,10 @@ class Schedule:
                 operation, 
                 max(prev_m_op.end if prev_m_op else 0, prev_j_op.end if prev_j_op else 0)
             )
+
+            if prev_j_op and prev_j_op.k > curr_op.k:
+                raise ValueError(f"Technological order of J{operation.j} was not maintained.")
+            
             m_ops[operation.m] = curr_op
             j_ops[operation.j] = curr_op
             sched_ops.append(curr_op)
@@ -82,39 +85,41 @@ class Schedule:
         return list(set([o.m for o in self.scheduled_operations]))
 
     @property
-    def tasks(self) -> list[int]:
+    def jobs(self) -> list[int]:
         return list(set([o.j for o in self.scheduled_operations]))
     
     @property
     def c_max(self) -> int:
         return max(o.end for o in self.scheduled_operations) if self.scheduled_operations else 0
 
-    def display(self) -> None:
+    def display(self, group_by: Literal['job', 'machine'] = 'machine', show_c_max: bool = True) -> None:
             if not self.scheduled_operations:
                 return
 
-            tasks = sorted(self.tasks)
-            machines = sorted(self.machines)
-
-            print()
-            print(color(f" OPERATIONS BY MACHINE ".center(50, '='), bg='#a9a9a9', fg='black', style='bold'))
-            for m in machines:
-                ops = sorted([o for o in self.scheduled_operations if o.m == m], key=lambda x: x.start)
-                
-                print(color(f" MACHINE {m} ".center(30, '-'), fg='black', bg='#d3d3d3'))
-                for op in ops:
-                    print(op)
-
-            print()
-            print(color(f" OPERATIONS BY TASK ".center(50, '='), bg='#a9a9a9', fg='black', style='bold'))
-            for t in tasks:
-                ops = sorted([o for o in self.scheduled_operations if o.j == t], key=lambda x: x.start)
-                
-                print(color(f" TASK {t} ".center(30, '-'), fg='black', bg='#d3d3d3'))
-                for op in ops:
-                    print(op)
+            match group_by:
+                case 'machine':
+                    machines = sorted(self.machines)
+                    print(color(f" OPERATIONS BY MACHINE ".center(50, '='), bg='#a9a9a9', fg='black', style='bold'))
+                    for m in machines:
+                        ops = sorted([o for o in self.scheduled_operations if o.m == m], key=lambda x: x.start)
+                        
+                        print(color(f" MACHINE {m} ".center(30, '-'), fg='black', bg='#d3d3d3'))
+                        for op in ops:
+                            print(op)
+                case 'job':
+                    jobs = sorted(self.jobs)
+                    print(color(f" OPERATIONS BY JOB ".center(50, '='), bg='#a9a9a9', fg='black', style='bold'))
+                    for t in jobs:
+                        ops = sorted([o for o in self.scheduled_operations if o.j == t], key=lambda x: x.start)
+                        
+                        print(color(f" JOB {t} ".center(30, '-'), fg='black', bg='#d3d3d3'))
+                        for op in ops:
+                            print(op)
+                case _:
+                    return
             
-            print(f"\n{color(f"C_max: {self.c_max}", fg='white', bg='#e6194B', style='bold')}\n")
+            if show_c_max:
+                print(f"\n{color(f"C_max: {self.c_max}", fg='white', bg='#e6194B', style='bold')}\n")
 
 
 def generate_random_operations(n, m, Z) -> list[Operation]:
@@ -146,11 +151,74 @@ def generate_random_operations(n, m, Z) -> list[Operation]:
             )
 
     return ret
+
+def insa(operations: Iterable[Operation], reinsert: Literal[None, 1, 2, 3, 4] = None) -> Schedule:
+    pi_star_schedule = Schedule(())
+    pi: tuple[Operation, ...] = ()
+    W: deque = deque(sorted(Schedule.from_operations(tuple(operations)).jobs, key=lambda j: sum(o.p for o in operations if o.j == j), reverse=True))
+
+    # TODO: correct this func so it looks for best insertion for the job (not greedily for the operation)
+    def insert_job(job: int):
+        nonlocal pi_star_schedule, pi
+        min_pos = 0
+        for operation in sorted((o for o in operations if o.j == job), key=lambda o: o.k):
+            pi_star_schedule = Schedule.from_operations(pi + (operation,))
+
+            for i in range(min_pos, len(pi)):
+                new_pi = pi[:i] + (operation,) + pi[i:]
+                try:
+                    new_pi_schedule = Schedule.from_operations(new_pi)
+                    if not pi_star_schedule.scheduled_operations or new_pi_schedule.c_max < pi_star_schedule.c_max:
+                        pi_star_schedule = new_pi_schedule
+                        min_pos = i + 1
+                except ValueError:
+                    continue
+            
+            pi = pi_star_schedule.scheduled_operations
+
+    while W:
+        job = W.popleft()
+        insert_job(job)
+
+        already_scheduled = set(o.j for o in pi) - {job}
+        x = None
+
+        match reinsert:
+            case 1|2|3:
+                raise NotImplementedError("TODO: implement missing methods")
+            case 4:
+                x_c_max = float('inf')
+                for candidate in already_scheduled:
+                    new_pi = tuple(o for o in pi if o.j != candidate)
+                    try:
+                        new_c_max = Schedule.from_operations(new_pi).c_max
+                        if new_c_max < x_c_max:
+                            x_c_max = new_c_max
+                            x = candidate
+                    except ValueError:
+                        continue
+            case _:
+                continue
+        
+        if x is not None:
+            pi = tuple(o for o in pi if o.j != x)
+            insert_job(x)
+
+    return pi_star_schedule
     
 
 if __name__ == '__main__':
     operations = generate_random_operations(n=11, m=7, Z=1)
 
-    print(color(f" Schedule #1: Original Task Order ".center(60, '#'), bg="#ffc800", fg='black', style='bold'))
+    print(color(f" Schedule #1: Original Operations Order ".center(70, '#'), bg="#ffc800", fg='black', style='bold'))
     operations_schedule = Schedule.from_operations(operations)
-    operations_schedule.display()
+    operations_schedule.display(group_by='job', show_c_max=False)
+    operations_schedule.display(group_by='machine', show_c_max=True)
+
+    print(color(f" Schedule #2: INSA Operations Order (no reinsert) ".center(70, '#'), bg="#ffc800", fg='black', style='bold'))
+    insa_schedule = insa(operations, reinsert=None)
+    insa_schedule.display(group_by='machine', show_c_max=True)
+
+    print(color(f" Schedule #3: INSA Operations Order (reinsert [4]) ".center(70, '#'), bg="#ffc800", fg='black', style='bold'))
+    insa_schedule = insa(operations, reinsert=4)
+    insa_schedule.display(group_by='machine', show_c_max=True)
